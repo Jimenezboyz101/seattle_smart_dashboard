a83546f4b06f8bf7ce2bf5a6b93015c31f722525
lizbethsarabia <lizbeth@uw.edu>
2026-02-28 00:54:21 -0800

    started cluster

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   map.on("load", () => {
-
+//api call to get collision data from SDOT and add it as a source to the map
     fetch("https://services.arcgis.com/ZOyb2t4B0UYuYNYH/arcgis/rest/services/SDOT_Collisions_All_Years/FeatureServer/0/query?where=1%3D1&outFields=INJURIES,SERIOUSINJURIES,FATALITIES,INCDATE&outSR=4326&f=geojson")
       .then(response => response.json())
       .then(data => {
@@ -19,9 +19,74 @@ document.addEventListener("DOMContentLoaded", () => {
         // Add GeoJSON source
         map.addSource("collisions", {
           type: "geojson",
-          data: data
+          data: data,
+          cluster: true, 
+          clusterMaxZoom: 14,
+          clusterRadius: 50
         });
 
+        //cluster layer
+        map.addLayer({
+          id: "clusters",
+          type: "circle",
+          source: "collisions",
+          filter: ["has", "point_count"],
+          layout: {
+              "visibility": "visible"
+          },
+          paint: {
+            "circle-color": [
+              "step",
+              ["get", "point_count"],
+              "#51bbd6",
+              100,
+              "#f1f075",
+              300,
+              "#f28cb1"
+            ],
+            "circle-radius": [
+              "step",
+              ["get", "point_count"],
+              18,
+              100, 28,
+              500, 38
+            ],
+            "circle-opacity": 0.85
+          }
+        });
+
+        //cluster count labels
+        map.addLayer({
+          id: "cluster-count",
+          type: "symbol",
+          source: "collisions",
+          filter: ["has", "point_count"],
+          layout: {
+            "text-field": "{point_count_abbreviated}",
+            "text-size": 12
+          },
+          paint: {
+            "text-color": "#000000"
+          }
+        });
+
+
+        map.on("click", "clusters", (e) => {
+          const features = map.queryRenderedFeatures(e.point, {
+            layers: ["clusters"]
+          });
+
+          const clusterId = features[0].properties.cluster_id;
+
+          map.getSource("collisions").getClusterExpansionZoom(clusterId, (err, zoom) => {
+            if (err) return;
+
+            map.easeTo({
+              center: features[0].geometry.coordinates,
+              zoom: zoom
+            });
+          });
+        });
       })
       .catch(error => {
         console.error("Error fetching collision data:", error);
